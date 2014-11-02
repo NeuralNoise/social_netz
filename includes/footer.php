03973952645fbadecec9482d668d48c9215e5f0a
faysal <faysalshahzad1@gmail.com>
2014-11-02 16:13:41 +0100

    footer message fix

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <?php include('variables/variables.php'); ?>
 <div id="footer">
- 	<p><?php echo $footer ?>Here comes the footer content</p>
+ 	<p><?php echo $footer."<br>Here comes the footer content" ?></p>
 </div>			<!-- end #footer -->
